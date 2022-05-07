a5d32ac846a3a1290f0a18ffb862f167f573830c
Karthikeyan Muthu <phenomenalkarthikeyan@gmail.com>
2022-05-08 00:05:09 +0530

    M

---
+++ python-sort-vs-sorted/python-sort-vs-sorted.py
@@ -0,0 +1,16 @@
+list_variable = [3,5,1,2,4]
+
+# Using sorted()
+print('Using sorted() built in function : ')
+print('Actual List content before sorting :' , list_variable)
+print('Sorted() :' , sorted(list_variable))
+print('Now printing the list content again :' , list_variable)
+# Seems sorted() does not change the actual list object content
+
+print('*' * 50)
+# Using list.sort()
+print('Using list.sort() built in list method : ')
+print('Actual List content before sorting :' , list_variable)
+print('Sorted() :' , list_variable.sort())
+print('Now printing the list content again :' , list_variable)
+# Seems list.sort() stored the sorted list content in the passed list object
