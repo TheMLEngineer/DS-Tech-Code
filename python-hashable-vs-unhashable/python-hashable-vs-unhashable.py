0e22cf325542df8b5b30261c56969b699caae56f
Karthikeyan Muthu <phenomenalkarthikeyan@gmail.com>
2022-05-10 22:08:25 +0530

    M

--- python-hashable-vs-unhashable/python-hashable-vs-unhashable.py
+++ python-hashable-vs-unhashable/python-hashable-vs-unhashable.py
@@ -1,11 +1,17 @@
 
-
 # String is hashable so hash function gives proper response
 string_variable = 'abc'
 print('String variable hash value :' , hash(string_variable))
 
+# ALl functions in Python is hashable
+def fn():
+  print('Hi')
+print('Hash Value of a function :' , hash(fn))
+
 # list is unhashable so hash function does not gives proper response
 list_variable = [1,2,3]
 print('List variable hash value :' , hash(list_variable))
+
+
 
 ##############################################################################################
