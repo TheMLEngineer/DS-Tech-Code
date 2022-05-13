bb45472b7cb2e5d32ec3e1dc14253e34266edd78
Karthikeyan Muthu <phenomenalkarthikeyan@gmail.com>
2022-05-13 20:21:51 +0530

    M

--- python-json/python-json.py
+++ python-json/python-json.py
@@ -5,11 +5,11 @@ f = open('sample.json').read()
 dictionary = json.loads(f)
 print(dictionary['name'])
 
-
-##############################################################################################
-
-##############################################################################################
-
-##############################################################################################
+# Converting dictionary to .json file ,
+# Check new_json.json file to check content
+json_string = json.dumps(dictionary , indent = 4)
+f = open('new_json.json' , 'w')
+f.write(json_string)
+f.close()
 
 ##############################################################################################
