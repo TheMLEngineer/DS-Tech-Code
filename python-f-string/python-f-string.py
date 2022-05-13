31eafdce79f327b1f82fc4294a624436f0961a1d
Karthikeyan Muthu <phenomenalkarthikeyan@gmail.com>
2022-05-13 22:28:44 +0530

    M

--- python-f-string/python-f-string.py
+++ python-f-string/python-f-string.py
@@ -1,17 +1,36 @@
 
 
-# f-string using 'f'
 name = 'Karthik'
+age = 24
+
+# f-string using 'f'
 print(f'I am {name}')
 
 # f-string using 'F'
 print(F'I am {name}')
 
+# Multiline f-string
+data = (
+    f"I am {name}"
+    F"My Age is {age}"
+)
+print('Multiline f-string :')
+print(data)
+
+# Expressions inside f-string
+print(f'3 + 5 = {3+5}' )
 
 ##############################################################################################
 
+import datetime
+today = datetime.datetime.today()
+print(f"{today:%B %d, %Y}")
 
 ##############################################################################################
 
+# Call Functions Inside f-string
+def fn():
+    print('Function is called ...')
+F'{fn()}'
 
 ##############################################################################################
