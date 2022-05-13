1a03a5e5443b1da42b07bb2fab2426eb54b37812
Karthikeyan Muthu <phenomenalkarthikeyan@gmail.com>
2022-05-13 22:33:24 +0530

    M

--- python-f-string/python-f-string.py
+++ python-f-string/python-f-string.py
@@ -1,5 +1,3 @@
-
-
 name = 'Karthik'
 age = 24
 
@@ -12,7 +10,7 @@ print(F'I am {name}')
 # Multiline f-string
 data = (
     f"I am {name}"
-    F"My Age is {age}"
+    F"\nMy Age is {age}"
 )
 print('Multiline f-string :')
 print(data)
@@ -20,10 +18,12 @@ print(data)
 # Expressions inside f-string
 print(f'3 + 5 = {3+5}' )
 
+
 ##############################################################################################
 
 import datetime
 today = datetime.datetime.today()
+# Using f-string for time formatting
 print(f"{today:%B %d, %Y}")
 
 ##############################################################################################
@@ -31,6 +31,9 @@ print(f"{today:%B %d, %Y}")
 # Call Functions Inside f-string
 def fn():
     print('Function is called ...')
+# Here we are not even assigining f-string to a variable
+# We just run f-string , it is not stored , but
+# expression inside fstring gets executed
 F'{fn()}'
 
 ##############################################################################################
