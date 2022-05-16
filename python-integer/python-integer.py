9bc1ac319d8d0f5857b0c929792e5face462adb9
Karthikeyan Muthu <phenomenalkarthikeyan@gmail.com>
2022-05-16 15:15:30 +0530

    M

---
+++ python-integer/python-integer.py
@@ -0,0 +1,21 @@
+# Integer
+variable = 3
+
+print('type of variable :' , type(variable))
+
+# Type conversion
+print('Changing int to float :' , float(variable))
+print('Changing into to complex :' , complex(variable))
+
+# underscore can be used as Delimter to integer
+variable = 1_2_3
+print(variable)
+
+# Binary , Octals and Hexadecimals
+binary = 0b11011
+octal = 0o12
+hexadecimal = 0x12
+
+print(binary , octal , hexadecimal)
+
+##############################################################################################
