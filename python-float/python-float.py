5be0d400b0a1eb3cb2d974fa30284f6428e2b200
Karthikeyan Muthu <phenomenalkarthikeyan@gmail.com>
2022-05-16 15:41:29 +0530

    M

---
+++ python-float/python-float.py
@@ -0,0 +1,19 @@
+
+# Float
+variable = 3.3
+
+print('type of variable :' , type(variable))
+
+# Type conversion
+print('Changing float to int :' , int(variable))
+print('Changing float to complex :' , complex(variable))
+
+# underscore can be used as Delimter to Float also
+variable = 3_2.3_5
+print(variable)
+
+# inf and nan
+print(float('NaN'))
+print(float('inf'))
+
+##############################################################################################
