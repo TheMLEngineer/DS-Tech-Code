4b1081267486dcc6b6955ef2cd4df5dc5963f8fc
Karthikeyan Muthu <phenomenalkarthikeyan@gmail.com>
2022-05-16 15:55:36 +0530

    M

---
+++ python-complex/python-complex.py
@@ -0,0 +1,18 @@
+# Complex
+variable = 3 + 0j
+
+print('type of variable :' , type(variable))
+
+print('Real part of variable :' , variable.real)
+print('Imaginary part of variable :' , variable.imag)
+
+
+# Type conversion
+# We can’t convert a complex number to int or float directly
+
+
+# underscore can be used as Delimter to Float also
+variable = 3_2 + 3_5j
+print(variable)
+
+##############################################################################################
