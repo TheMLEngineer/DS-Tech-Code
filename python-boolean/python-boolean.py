75dcd58ca323d1a08c9c8878658502272a1d0388
Karthikeyan Muthu <phenomenalkarthikeyan@gmail.com>
2022-05-21 23:27:33 +0530

    M

--- python-boolean/python-boolean.py
+++ python-boolean/python-boolean.py
@@ -1,9 +1,37 @@
 
+# Booleans
+# Arithmetic operations on boolean
+print('True + True =' , True + True)
+print('True + False =' , True + False)
+print('False + False =' , False + False)
 
+# Boolean value of None
+print('Boolean value of None :' , bool(None))
+
+# Control flow using Boolean
+# if logic
+if True:
+    print('This line is executed ...')
+else:
+    print('This line will never be executed as if loop condition check is always True')
+
+# while logic
+var = 1
+while var <= 5:
+    print('Variable value :' , var)
+    var += 1
 
 ##############################################################################################
 
-
-##############################################################################################
+# Booleans
+# Boolean type conversion
+print('Integer conversion of True :' , int(True))
+print('Integer conversion of False :' , int(False))
+print('Float conversion of True :' , float(True))
+print('Float conversion of False :' , float(False))
+print('Imaginary number conversion of True :' , complex(True))
+print('Imaginary number conversion of False :' , complex(False))
+print('String conversion of True :' , str(True))
+print('String conversion of False :' , str(False))
 
 ##############################################################################################
