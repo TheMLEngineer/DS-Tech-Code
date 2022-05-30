875b6e2ac51343e9dd8775977230526bc89f692c
Karthikeyan Muthu <phenomenalkarthikeyan@gmail.com>
2022-05-30 17:59:51 +0530

    M

---
+++ python-iterators-vs-iterables/python-iterators-vs-iterables.py
@@ -0,0 +1,9 @@
+# Iterable
+iterable_object = [1,2,3]
+# Iterator
+iterator_object = iter([1,2,3])
+
+print(iterable_object)
+print(iterator_object)
+
+##############################################################################################
