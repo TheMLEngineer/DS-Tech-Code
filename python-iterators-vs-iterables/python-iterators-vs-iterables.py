152648c3c18aa35a4354ea07e3f0724b0ff7587e
Karthikeyan Muthu <phenomenalkarthikeyan@gmail.com>
2022-06-04 02:12:06 +0530

    M

--- python-iterators-vs-iterables/python-iterators-vs-iterables.py
+++ python-iterators-vs-iterables/python-iterators-vs-iterables.py
@@ -3,7 +3,22 @@ iterable_object = [1,2,3]
 # Iterator
 iterator_object = iter([1,2,3])
 
-print(iterable_object)
-print(iterator_object)
+print('iterable_object :' , iterable_object)
+print('iterator_object :' , iterator_object)
+
+##############################################################################################
+
+my_string = "Karthik"
+
+try:
+    a = next(my_string)
+except Exception as e:
+    print('Exception has occured ...')
+    print(e)
+
+# Converting iterable to iterator
+iterator_string = iter(my_string)
+a = next(iterator_string)
+print('First Value :' , a)
 
 ##############################################################################################
