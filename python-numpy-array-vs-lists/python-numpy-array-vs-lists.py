7b8dc8c4263a60df4d0a1794a3f29056d0e8f6a7
Karthikeyan Muthu <phenomenalkarthikeyan@gmail.com>
2022-06-04 18:39:26 +0530

    M

--- python-numpy-array-vs-lists/python-numpy-array-vs-lists.py
+++ python-numpy-array-vs-lists/python-numpy-array-vs-lists.py
@@ -25,13 +25,20 @@ print('Appending in Numpy Array Duration: {}'.format(end_time - start_time))
 
 ##############################################################################################
 
-
+import numpy as np
+import sys
+print('Size of 1000 element list :' , sys.getsizeof([1]))
+print('Size of 1000 element numpy array :' , sys.getsizeof(np.array([1])))
 
 
 ##############################################################################################
 
 
-
+# numpy array vs list
+# Numpy can only be homogenous
+import numpy as np
+np_array = np.array(['1' , 2 , 3.3])
+print('All values are now in string dtype :' , np_array)
 
 ##############################################################################################
 
