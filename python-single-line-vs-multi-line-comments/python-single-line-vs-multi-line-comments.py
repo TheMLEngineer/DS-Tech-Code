65543584aefa1e5ea683f88230eebf3627102fd2
Karthikeyan Muthu <phenomenalkarthikeyan@gmail.com>
2022-06-06 15:17:45 +0530

    M

---
+++ python-single-line-vs-multi-line-comments/python-single-line-vs-multi-line-comments.py
@@ -0,0 +1,22 @@
+
+# This is a Single Line Comment
+print('Hi')
+
+##############################################################################################
+
+print('Hi') # This is an Inline Comment
+
+##############################################################################################
+
+# This is a Multi Line Comment
+# It can be achieved by using triple quotes ''' or """
+# Or can be achieved by multiple single line comments
+
+'''
+This is a 
+Multi Line Comment
+It can be achieved by using triple quotes
+'''
+print('Hi')
+
+##############################################################################################
