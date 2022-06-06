405db885e721a23343780c395a312ebd7a00eb0d
Karthikeyan Muthu <phenomenalkarthikeyan@gmail.com>
2022-06-06 16:24:29 +0530

    M

---
+++ python-one-line-statements-vs-multi-line-statements/python-one-line-statements-vs-multi-line-statements.py
@@ -0,0 +1,15 @@
+
+
+# Single line statement
+a = 1
+
+# Multi line statement
+b = a *\
+    2 +\
+    3
+
+# Converting multi line statement to single line statement using ;
+print('A Value :' , a); print('B Value :' , b)
+
+
+##############################################################################################
