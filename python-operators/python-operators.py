1eb50a4b94ff61bb8e52209b980276ee5d87e3b4
Karthikeyan Muthu <phenomenalkarthikeyan@gmail.com>
2022-08-03 17:05:36 +0530

    commit

--- python-operators/python-operators.py
+++ python-operators/python-operators.py
@@ -1,6 +1,6 @@
 
 
-# Arithmatic
+# Arithmatic Operators
 a = 3
 b = 5
 
@@ -16,7 +16,19 @@ print('a // b :' , a // b)
 
 ##############################################################################################
 
+# Comparison Operators
 
+a = 3
+b = 5
+
+print('a value :' , a)
+print('b value :' , b)
+print('Is a < b ? ' , a < b)
+print('Is a > b ? ' , a > b)
+print('Is a <= b ? ' , a <= b)
+print('Is a >= b ? ' , a >= b)
+print('Is a == b ? ' , a == b)
+print('Is a != b ? ' , a != b)
 
 
 
